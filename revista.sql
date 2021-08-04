d881d3bbaf653b422057c0cea5c158d2fe9cc236
Luis Heskey <lheskey@arsmetasalud.com>
2021-08-04 08:06:03 -0400

    [add]archive db

---
+++ revista.sql
@@ -0,0 +1,35 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               10.4.20-MariaDB - mariadb.org binary distribution
+-- Server OS:                    Win64
+-- HeidiSQL Version:             11.3.0.6295
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+-- Dumping database structure for dbrevista
+CREATE DATABASE IF NOT EXISTS `dbrevista` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+USE `dbrevista`;
+
+-- Dumping structure for table dbrevista.articulos
+CREATE TABLE IF NOT EXISTS `articulos` (
+  `IdArticulo` int(11) NOT NULL AUTO_INCREMENT,
+  `Titulo` varchar(50) NOT NULL,
+  `Contenido` longtext NOT NULL,
+  `FechaCreacion` date NOT NULL DEFAULT sysdate(),
+  `PublicadoPor` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`IdArticulo`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- Data exporting was unselected.
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
